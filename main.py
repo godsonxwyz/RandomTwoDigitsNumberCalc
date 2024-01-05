19698ddd0dae32fdcc2cd25acc4fb8273aa3c398
godsonxwyz <godspowerpatrick502@gmail.com>
2024-01-05 03:08:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+# Data types of 2 random numbers
+# Subscripting
+# Think about data type conversion
+
+two_numbers = input("Type a two digit number:\n")
+
+#
+#
+first_digit = int(two_numbers[0])
+
+second_number = int(two_numbers[1])
+
+
+print(first_digit + second_number)
+
